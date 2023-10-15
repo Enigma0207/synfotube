055bb485dd3fe85e1806a51dbbd51f1e29353387
Enigma0207 <alinmansita@gmail.com>
2023-10-15 23:50:25 +0200

    pagination

--- src/Controller/IngredientController.php
+++ src/Controller/IngredientController.php
@@ -3,6 +3,9 @@
 namespace App\Controller;
 
 use App\Repository\IngredientRepository;
+use Knp\Component\Pager\PaginatorInterface;
+use Doctrine\ORM\Tools\Pagination\Paginator;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -11,14 +14,17 @@ class IngredientController extends AbstractController
 {
     #[Route('/ingredient', name: 'ingredient')]
     // injection de dependance cad injecter un service dans le parametre du controller (IngredientRepository $repository)
-    public function index(IngredientRepository $repository): Response
+    public function index(IngredientRepository $repository,PaginatorInterface $paginator, Request $request): Response
     {
         $ingredients = $repository->findAll();//apl a la fonction findAll depuis ingredientRepository
         // dd($ingredients); si on veut voir ce quilya dans $ingredients 
+        $ingredients = $paginator->paginate(
+            $repository->findAll(), 
+            $request->query->getInt('page', 1), // Page actuelle, pouvant être passé en paramètre d'URL
+            10 // Nombre d'éléments par page
+        );
         return $this->render('pages/ingredient/index.html.twig', [
-            
             'ingredients'=> $ingredients
-            // ou bien on supprime la ligne 16, et la lige 20 devient  'ingredients'=>$repository->findAll()
         ]);
     }
 }
